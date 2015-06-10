2d4cf426aa2c0f0d0d0125fd7d91d2fb9d30eb8a
Robciuc Ioan <ionut@9pixels.co>
2015-06-10 20:03:26 +0300

    Issue (header.php - charset) fixed
    Issue (header.php - charset) fixed.

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 		<meta http-equiv="<?php echo get_template_directory_uri();?>/content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-		<meta charset="UTF-8">
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<title><?php wp_title('|', true, 'right'); ?></title>
 		
 		<?php wp_head(); ?>
