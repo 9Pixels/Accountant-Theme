4430360c2d6ea12e30dc88e19efffd0656389f7f
Robciuc Ioan <ionut@9pixels.co>
2015-07-20 10:23:32 +0300

    Issue (Can you describe the purpose of using linking http-equiv= HTML parameter.) fixed.

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-		<meta http-equiv="<?php echo get_template_directory_uri();?>/content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<?php wp_head(); ?>
 	</head>
